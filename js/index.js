bf06625ebb6b5bd11a40d4e78ffc25ccfe234ece
Isaiah van Hunen <i.van.hunen@student.rug.nl>
2019-10-05 14:56:13 +0200

    Added colour switch

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+	setTimeout(function(){
+		$('input[name=nameField]').focus();
+	}, 1500);
+
 	$('input[name=nameField]').keyup(function(e){
 		if(e.keyCode == 13){
 			$(this).prop("disabled", true).val($(this).val()+".");
-			$("#nameFieldHolder").delay(1000).addClass("col-10").removeClass("col-12");
+			$("#nameFieldHolder").addClass("col-10").removeClass("col-12");
 			$("#prefixText").delay(1750).animate({
 				'margin-left' : "0%",
 				'opacity' : '1',
 			}, 500);
-			$("#greetingResponse").delay(2750).animate({opacity: 1}, 1000);
+			setTimeout(function(){
+				$("body").removeClass("bg-dark").addClass("bg-greenish");
+			}, 2500);
+			$("#greetingResponse").delay(3750).animate({opacity: 1}, 1000);
 		}
 	});
 
