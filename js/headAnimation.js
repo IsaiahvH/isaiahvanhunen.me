ee26ea0e677bd4633c0157490304dd3df265d572
Isaiah van Hunen <i.van.hunen@student.rug.nl>
2019-10-07 09:10:30 +0200

    Added blink and reset

--- js/headAnimation.js
+++ js/headAnimation.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-	$("#headSVG").on( "load", function(){
-		var svgDoc = this.contentDocument;
-		var svgRoot  = svgDoc.documentElement;
+	$("#headSVG").on("load", function(){
+		var SVGRoot  = this.contentDocument.documentElement;
 		
 		$(document).mousemove(function(e){
 			w = $(window);
 			xOff = (e.clientX/w.width()-0.5)*16;
 			yOff = (e.clientY/w.height()-0.5)*8;
-			$("#leftEye, #rightEye", svgRoot).attr("transform", "translate("+xOff+" "+yOff+")");
+			$("#leftEye, #rightEye", SVGRoot).attr("transform", "translate("+xOff+" "+yOff+")");
 		});
 		$(document).mousedown(function(e){
-			$("#rightEye", svgRoot).addClass("blink");
+			$("#rightEye", SVGRoot).addClass("blink");
 		});
 		$(document).mouseup(function(e){
-			$("#rightEye", svgRoot).removeClass("blink");
+			$("#rightEye", SVGRoot).removeClass("blink");
 		});
 	});
 });
--- js/introAnimation.js
+++ js/introAnimation.js
@@ -1,27 +1,79 @@
+if(localStorage.getItem("name") != null){
+	$("body, .form-label-group > label, .form-label-group input").css("transition", "none");
+	introduceMyself(false, localStorage.getItem("name"));
+}
+
 $(document).ready(function() {
 	setTimeout(function(){
 		$('input[name=nameField]').focus();
 	}, 1500);
 
-	$('input[name=nameField]').keyup(function(e){
-		if(e.keyCode == 13){
-			$(this).prop("disabled", true).val($(this).val()+".");
-			$("#nameFieldHolder").addClass("col-10").removeClass("col-12");
-			$("#headSVG").delay(1000).animate({opacity: 1}, 500);
-			$("#prefixText").delay(1750).animate({
-				'margin-left' : "0%",
-				'opacity' : '1',
-			}, 500);
-			setTimeout(function(){
-				$("body").removeClass("bg-dark").addClass("bg-greenish");
-			}, 2500);
-			$("#greetingResponse").delay(3750).animate({opacity: 1}, 1000);
-		}
-	});
+	if(localStorage.getItem("name") == null){
+		$("#headSVG").on("load", function(){
+			var svgElem = this;
+			$('input[name=nameField]').keyup(function(e){
+				if(e.keyCode == 13)
+					introduceMyself(svgElem);
+			});
+		});
+	}
 
 	$("#talkBtn").click(function(){
 		$(this).prop("disabled", true)
 		$("#talkResponse1").css('visibility','visible').hide().slideDown();
 		$("#talkResponse2").delay(3000).css('visibility','visible').hide().slideDown();
 	});
+
+	$("#resetGreeting").click(function(){
+		localStorage.removeItem("name");
+		location.reload();
+	})
 });
+
+function introduceMyself(svgElem, name = false){
+	if(svgElem)
+		var SVGRoot = svgElem.contentDocument.documentElement;
+	if(name){
+		$("input[name=nameField]").val(name);
+	} else {
+		localStorage.setItem("name", $("input[name=nameField]").val());
+	}
+	$("input[name=nameField]").prop("disabled", true);
+	$("#nameFieldHolder").addClass("col-10").removeClass("col-12");
+
+	var TC = name ? 0 : 1;
+
+	// Animate head
+	var headDelay = TC*1000;
+	$("#headSVG").delay(headDelay).animate({opacity: 1}, TC*500);
+
+	// Animate introduction
+	var introDelay = headDelay+TC*750
+	$("#prefixText").delay(introDelay).animate({
+		'margin-left' : "0%",
+		'opacity' : '1',
+	}, TC*500);
+	$("input[name=nameField]").delay(introDelay+TC*600).queue(function(){
+		$(this).val($(this).val()+".").dequeue();
+	});
+
+	// Blink
+	if(svgElem){
+		var blinkDelay = introDelay+TC*1000
+		$("#rightEye", SVGRoot).delay(blinkDelay).queue(function(){
+			$(this).addClass("blink").dequeue();
+		}).delay(200).queue(function(){
+			$(this).removeClass("blink").dequeue();
+		});
+	}
+
+	// Animate body
+	var bodyDelay = blinkDelay+TC*1000;
+	$("body").delay(bodyDelay).queue(function(){
+		$(this).removeClass("bg-dark").addClass("bg-greenish");
+	});
+
+	// Animate response
+	$("#greetingResponse").delay(bodyDelay + TC*500).animate({opacity: 1}, TC*1000);
+	$("#resetGreeting").delay(bodyDelay + TC*1500).fadeIn();
+}
